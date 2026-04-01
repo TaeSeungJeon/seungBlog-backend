7794669c132178255a1ce99e5f2e79f2d47c0c95
TaeSeungJeon <xoxoxx832@gmail.com>
2026-04-01 09:24:32 +0900

    feat: 프로젝트 초기 셋팅

---
+++ src/main/java/com/playground/backend/TaeSeungJeonApplication.java
@@ -0,0 +1,13 @@
+package com.playground.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaeSeungJeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaeSeungJeonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playground/backend/TaeSeungJeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaeSeungJeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
